ca3bd40a36527837a636d4f060da8544a29d61b4
John Hagens <ginuwine104@gmail.com>
2023-09-22 06:13:31 -0400

    debug7

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -10,7 +10,8 @@
     <title>ICI Homes Landing Page</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}"> 
+    <link rel="stylesheet" href="{{ app()->isLocal() ? asset('css/app.css') : secure_asset('css/app.css') }}" />
+
 </head>
 <body>
     <section class="video-section">
