66ec72bcfdea9ff4b91134881c770a0585819b55
John Hagens <ginuwine104@gmail.com>
2023-09-22 04:45:30 -0400

    debug2

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -143,7 +143,7 @@
         <script src="{{ asset('js/app.js') }}"></script>
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.0/dist/cdn.min.js"></script>
 
-        <script src="{{ asset('build/assets/app-4cfc8f67.js') }}"></script>
+       
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
