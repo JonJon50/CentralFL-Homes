38df21c40e1970da4567200cd806c2a455f383f4
John Hagens <ginuwine104@gmail.com>
2023-10-25 18:14:06 -0400

    landing.blade: script305

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,4 +1,4 @@
-<!-- landing.blade.php John Hagens-->
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -302,9 +302,9 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="{{ app()->environment('local') ? asset('js/app.js') : secure_asset('js/app.js') }}"></script>
+<!-- <script src="{{ app()->environment('local') ? asset('js/app.js') : secure_asset('js/app.js') }}"></script> -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <!-- i will do a process.env api key Environment Variable  -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCU6NBGVDW5MYmWfvmRkJEwIHUJBfo-qLc&libraries=places&callback=initMap" defer></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
