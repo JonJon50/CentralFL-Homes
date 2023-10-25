0006890345bb658c563a22079c7d12f2e3d40875
John Hagens <ginuwine104@gmail.com>
2023-10-25 16:52:52 -0400

    working debugging1

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -307,6 +307,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="{{ app()->environment('local') ? asset('js/app.js') : secure_asset('js/app.js') }}"></script>
 <!-- i will do a process.env api key Environment Variable  -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCU6NBGVDW5MYmWfvmRkJEwIHUJBfo-qLc&libraries=places&callback=initMap" defer></script>
 <script src="https://maps.googleapis.com/maps/api/js?key={{ config('services.google_maps.key') }}&callback=initMap&libraries=&v=weekly" defer></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
