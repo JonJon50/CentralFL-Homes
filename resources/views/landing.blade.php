b48dba117ed1157ebaccde50fa4628dbf12d20c2
John Hagens <ginuwine104@gmail.com>
2023-10-25 17:50:43 -0400

    removed secure

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -302,10 +302,9 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="{{ app()->environment('local') ? asset('js/app.js') : secure_asset('js/app.js') }}"></script>
+<script src="{{ app()->environment('local') ? asset('js/app.js') : asset('js/app.js') }}"></script>
 <!-- i will do a process.env api key Environment Variable  -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCU6NBGVDW5MYmWfvmRkJEwIHUJBfo-qLc&libraries=places&callback=initMap" defer></script>
-<!-- <script src="https://maps.googleapis.com/maps/api/js?key={{ config('services.google_maps.key') }}&callback=initMap&libraries=&v=weekly" defer></script> -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
